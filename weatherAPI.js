b5a246c5d3513c17b27f666a23d6e215287180b9
Carbine28 <Koch@tcd.ie>
2022-10-19 23:26:03 +0100

    more sorting

--- weatherAPI.js
+++ weatherAPI.js
@@ -8,7 +8,7 @@ const port = 8080;
 const apiKey = "8fd481d84b99229254d57833341a80cb";
 const searchLimit = 1;
 const nDayForecast = 4;
-const   numForecast = nDayForecast * 8; // 5 day 3 hour forecast. 8 Forecast per day. Weather API Max is 5 days. 
+const numForecast = nDayForecast * 8; // 5 day 3 hour forecast. 8 Forecast per day. Weather API Max is 5 days. 
 
 const publicPath = path.resolve(__dirname, "public"); // Return absolute path to public dir 
 
@@ -45,7 +45,7 @@ async function getWeather(req,res){
     }
 
     let sortedData = sortWeatherData(jsonData);
-    res.json(sortedData);
+    //res.json(sortedData);
 }
 
 function sortWeatherData(jsonObj){
@@ -53,8 +53,71 @@ function sortWeatherData(jsonObj){
     // Sort weatherData into 4 days.
     // bool for packing rain
     // 
+    let dayList = createDays(nDayForecast);
+    //console.log(dayList);
+    let sortedList = setDayData(dayList,jsonObj)
 
     // Should return a json string object.
+}
+
+function setDayData(myList,jsonObj){
+    
+    let dayIndex = 0;
+    let dayCounter = 0;
+
+    //console.log(myList[0].avgItems);
+    for (let i = 0; i < numForecast; i++){
+        
+        // Increase by 1 day for every 7 loops // Reset all variables
+        if(dayCounter === 8){
+            dayCounter = 0;
+            // windSpeed = (windSpeed / 7).toFixed(2);
+            // //windSpeed = parseInt(windSpeed);
+            // console.log(windSpeed);
+            //myList[dayIndex].avgItems.avgWind = windSpeed;
+            //console.log(tempList[dayIndex].avgItems.avgWind)
+            dayIndex++;
+            
+        }
+        if('rain' in jsonObj.weatherData[i]){
+            if(!myList[dayIndex].isRaining) myList[dayIndex].isRaining = true;
+            myList[dayIndex].avgItems.avgRain +=  jsonObj.weatherData[i].rain;
+            console.log(jsonObj.weatherData[i].rain)
+        }
+        
+        myList[dayIndex].avgItems.avgWind +=  jsonObj.weatherData[i].wind.speed;
+        
+
+        dayCounter++;
+    }   
+    console.log(myList);
+    return myList;   
+}
+
+function createDays(numDays){
+    
+    let dayList = [];
+    let day = {
+        "avgItems" : {
+            "avgWind" :  0,
+            "avgTemp" :  0,
+            "avgRain" :  0,
+            "avgPol"  :  0
+        },
+        "weatherType"  : "Null",
+        "packUmbrella" : false,
+        "isRaining"    : false
+    }
+    
+    let clone;
+    while(numDays){
+        clone = JSON.parse(JSON.stringify(day));
+        dayList.push(clone);
+        numDays--;
+    }
+    //console.log(JSON.stringify(dayList));
+    clone = JSON.parse(JSON.stringify(dayList))
+    return(clone);
 }
 
 //Remove async word here cause no use.
